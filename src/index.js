04172df3d3f051d066cc79832d650955c2952781
Antonenko Olexiy <antonenkoolexiy.com>
2020-10-11 12:36:41 +0300

    Hashrouter

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { BrowserRouter} from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store= {store}>
-      <BrowserRouter basename= {process.env.PUBLIC_URL}>
+      <HashRouter >
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>, document.getElementById('root')
 );
